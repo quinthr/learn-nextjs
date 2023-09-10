99e07f1817759c8dff10a656cfb9f0f666fb8a64
Quinth Anthony A. Razuman <31249145+quinthr@users.noreply.github.com>
2023-09-10 17:47:12 +0800

    Ignore build errors and env update

--- intro/fullstack-app/fullstack-app/next.config.js
+++ intro/fullstack-app/fullstack-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     appDir: true,
     serverComponentsExternalPackages: ["bcrypt"],
   },
